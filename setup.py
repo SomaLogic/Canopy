e7f41322a6f170df39e82d9c03a813c8c5d3fd7a
Joey Allison <jallison@somalogic.com>
2020-10-27 09:50:58 -0600

    Sync with bitbucket canopy 2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
 setup(
     name='canopy',
     python_requires='>3.8.0',
-    version='0.2',
+    version='0.2.1',
     packages=find_packages(),
     url='http://confluence.sladmin.com/display/INGEN/Canopy',
     license='',
@@ -14,13 +14,14 @@ setup(
     author_email='helpdesk@somalogic.com',
     description='SomaLogic Python Data Analysis Library',
     long_description=long_description,
+    include_package_data=True,
     scripts=[
         'bin/canopy_concat_adats',
         'bin/canopy_check_adat',
         'bin/canopy_smart_concat_adats',
     ],
     install_requires=[
-        'pandas>=1.0.0',
+        'pandas>=1.1.0',
         'numpy>=1.18.1',
     ],
 )
